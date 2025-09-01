7810660fe04aef9616562bcc9e33edebb3b26d5f
RajatManohara30 <rajat.manoharacse2024@indoreinstitute.com>
2025-09-01 11:21:21 +0530

    Update sum of square of natural number

--- sum of square of natural number.c
+++ sum of square of natural number.c
@@ -1,2 +1,13 @@
 #include<stdio.h>
-vi
+void main()
+{
+	int n,i,sum = 0;
+	printf("enter a number = ");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{ 
+		sum = sum + i*i;
+	}
+	printf("sum of square of natural number = %d ",sum);
+}
+
